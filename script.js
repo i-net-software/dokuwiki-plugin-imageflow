cc250fec714deb1959c0dfe6db098746d1afb2a0
Gerry Weißbach <gerry.w@gammaproduction.de>
2014-09-04 08:44:21 +0200

    Check if this has already been registered
    e.g. PopUpViewer would use this.

--- script.js
+++ script.js
@@ -711,20 +711,20 @@
 	};
 	
 	$(function(){
-		function img(url) {
-			var i = new Image();
-			i.src = url;
-			return i;
-		}
-	 
+	
+	    if ( typeof $.fn.naturalWidth == 'undefined' && typeof $.fn.naturalHeight == 'undefined' ) { return; }
+	
+		function img(url) { var i = new Image(); i.src = url; return i; }
 		if ('naturalWidth' in (new Image())) {
 			$.fn.naturalWidth  = function() { return this[0].naturalWidth; };
 			$.fn.naturalHeight = function() { return this[0].naturalHeight; };
 			return;
 		}
+
 		$.fn.naturalWidth  = function() { return img(this.src).width; };
 		$.fn.naturalHeight = function() { return img(this.src).height; };
 	});
+
 	
 	$(function(){
 		$('div.imageflow_wrapper').each(function(){
