4585fffbb616fbbc9ffaed2540954d1ec8cf96eb
Gerry Weißbach <gerry.w@gammaproduction.de>
2026-01-21 11:42:40 +0100

    Remove deprecated require_once() - use autoloading instead

--- action.php
+++ action.php
@@ -10,9 +10,6 @@
 // must be run within Dokuwiki
 if(!defined('DOKU_INC')) die();
 
-if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-require_once(DOKU_PLUGIN.'action.php');
-
 class action_plugin_imageflow extends DokuWiki_Action_Plugin {
 
     private $functions = null;
